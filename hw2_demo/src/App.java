6124b59112fee6abb8eafc4396c29a2a0f43988c
Berkant Agur <berkantagur68@gmail.com>
2025-12-01 21:04:32 +0100

    Add array and print its elements

--- hw2_demo/src/App.java
+++ hw2_demo/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int[] numbers = { 3, 7, 1, 9, 4 };
+
+        System.out.println("Table Values:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Index " + i + ": " + numbers[i]);
+        }
     }
 }
